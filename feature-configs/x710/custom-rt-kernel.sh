d7f209dee162a905f23be3588a28d41861435b89
Jim Ramsay <jramsay@redhat.com>
2020-12-09 11:57:46 -0500

    Update kernel URIs

--- feature-configs/x710/custom-rt-kernel.sh
+++ feature-configs/x710/custom-rt-kernel.sh
@@ -1,7 +1,7 @@
 sudo rpm-ostree override \
 	remove kernel{,-core,-modules,-modules-extra} \
-	--install http://download.eng.bos.redhat.com/brewroot/work/tasks/9152/33029152/kernel-rt-core-4.18.0-193.31.1.rt13.81.el8_2.ocptest.x86_64.rpm \
-	--install http://download.eng.bos.redhat.com/brewroot/work/tasks/9152/33029152/kernel-rt-modules-4.18.0-193.31.1.rt13.81.el8_2.ocptest.x86_64.rpm \
-	--install http://download.eng.bos.redhat.com/brewroot/work/tasks/9152/33029152/kernel-rt-modules-extra-4.18.0-193.31.1.rt13.81.el8_2.ocptest.x86_64.rpm \
-	--install http://download.eng.bos.redhat.com/brewroot/work/tasks/9152/33029152/kernel-rt-modules-internal-4.18.0-193.31.1.rt13.81.el8_2.ocptest.x86_64.rpm
+	--install http://karmatron.mooo.com/kernel-rt/kernel-rt-core-4.18.0-193.31.1.rt13.81.el8_2.ocptest.x86_64.rpm \
+	--install http://karmatron.mooo.com/kernel-rt/kernel-rt-modules-4.18.0-193.31.1.rt13.81.el8_2.ocptest.x86_64.rpm \
+	--install http://karmatron.mooo.com/kernel-rt/kernel-rt-modules-extra-4.18.0-193.31.1.rt13.81.el8_2.ocptest.x86_64.rpm \
+	--install http://karmatron.mooo.com/kernel-rt/kernel-rt-modules-internal-4.18.0-193.31.1.rt13.81.el8_2.ocptest.x86_64.rpm
 
