c62c962e097583c927e6fd093608a97e9b00ba4f
Vitaly Grinberg <vgrinber@redhat.com>
2020-12-22 17:49:26 +0200

    clone

--- feature-configs/cn-ran-overlays/utils/clone.sh
+++ feature-configs/cn-ran-overlays/utils/clone.sh
@@ -6,12 +6,12 @@ if [[ -z $1 || -z $2 || -z $3 ]]; then
   echo "Usage:"
   echo "./clone.sh <path> <from> <to>"
   echo "<path> - path to the <from> or the <to>,\
-  for example '../'"
+  for example '..'"
   echo "<from> - the name of the source forder,\
   for example 'du-ldc'"
   echo "<to> - the name of the destination forder,\
   for example du-ldc-2"
-  echo 'For example: "./clone.sh ../ du-ldc du-ldc-2"'
+  echo 'For example: "./clone.sh .. du-ldc du-ldc-2"'
   exit 1
 fi
 
@@ -22,6 +22,15 @@ NR_TO=$(echo $3 | sed -e 's/-/_/')
 
 cp -rf $1/$2 $1/$3
 
-find $1/$3 -type f -exec sed -i -e "'s/'$2'/'$3'/g'" {} +
-find $1/$3 -type f -exec sed -i -e "'s/'$NR_FROM'/'$NR_TO'/g'" {} +
-find $1/$3 -name '*'$2'*' -exec rename '*'$2'*' '*'$3'*' {} +
+find $1/$3 -type f -exec sed -i -e 's/$2/$3/g' {} +
+find $1/$3 -type f -exec sed -i -e 's/$NR_FROM/$NR_TO/g' {} +
+
+RN_FILES=$(find $1/$3 -type f -name *$2*)
+echo $RN_FILES
+for f in $RN_FILES; do 
+  PN=$(dirname $f)
+  FN=$(basename $f)
+  NEW_FN=$(echo $FN | sed -e s/$2/$3/)
+  echo $NEW_FN
+done
+
