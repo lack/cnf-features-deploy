34b0be882fb790c216f9aa2439b1087f5fa561ca
Vitaly Grinberg <vgrinber@redhat.com>
2020-12-22 09:09:29 +0200

    rwe

--- feature-configs/cn-ran-overlays/du-ldc/is_ready.sh
+++ feature-configs/cn-ran-overlays/du-ldc/is_ready.sh
@@ -1,8 +1,33 @@
 #!/bin/sh
 
-# no output means that the new machine config wasn't picked by MCO yet
-if [ -z "$(oc get mcp worker-du-ldc -o jsonpath='{.spec.configuration.source[?(@.name=="load-sctp-module")].name}')" ]; then
+READY=0
+function is_mcp_ready ()
+{
+    # no output means that the new machine config wasn't picked by MCO yet
+    if [ -z "$(oc get mcp worker-du-ldc -o jsonpath='{.spec.configuration.source[?(@.name=="$1")].name}')" ]; then
+        if [[ -n "${VERBOSE}" ]]; then
+            echo "$1 not picked"
+        fi
     exit 1
 fi
 
-oc wait mcp/worker-cnf --for condition=updated --timeout 1s
+is_mcp_ready("load-sctp-module")
+
+# }
+# # SCTP MC patch
+# if [ -z "$(oc get mcp worker-du-ldc -o jsonpath='{.spec.configuration.source[?(@.name=="load-sctp-module")].name}')" ]; then
+#     exit 1
+# fi
+
+# # Disable chronyd patch
+# if [ -z "$(oc get mcp worker-du-ldc -o jsonpath='{.spec.configuration.source[?(@.name=="disable-chronyd-du-ldc")].name}')" ]; then
+#     exit 1
+# fi
+
+# # Performance profile
+# if [ -z "$(oc get mcp worker-du-ldc -o jsonpath='{.spec.configuration.source[?(@.name=="performance-perf-du-ldc")].name}')" ]; then
+#     exit 1
+# fi
+
+
+# oc wait mcp/worker-cnf --for condition=updated --timeout 1s
