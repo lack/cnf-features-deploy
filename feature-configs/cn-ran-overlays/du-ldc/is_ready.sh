43e94a839891a9b8deafca55974ae643e6af98fc
Vitaly Grinberg <vgrinber@redhat.com>
2020-12-22 10:57:23 +0200

    Write is_ready script for du-ldc

--- feature-configs/cn-ran-overlays/du-ldc/is_ready.sh
+++ feature-configs/cn-ran-overlays/du-ldc/is_ready.sh
@@ -1,33 +1,46 @@
 #!/bin/sh
+#
+# Check DU-LDC deployment / configuration are complete
 
-READY=0
+
+####################################################
+# Checks whether the machine config pool contains a 
+#   specific module
+# Globals:
+#   VERBOSE - if defined, prints verbose status
+# Arguments:
+#   MC module name to check for presense
+# Outputs:
+#   None
+#   Aborts the script if the module is not present
+####################################################
 function is_mcp_ready ()
 {
-    # no output means that the new machine config wasn't picked by MCO yet
-    if [ -z "$(oc get mcp worker-du-ldc -o jsonpath='{.spec.configuration.source[?(@.name=="$1")].name}')" ]; then
+    MODULE=\"$1\"
+    QUERY="{.spec.configuration.source[?(@.name==$MODULE)].name}"
+    # If module name is not present in the MCP, abort (not ready)
+    if [ -z "$(oc get mcp worker-du-ldc -o jsonpath=$QUERY)" ]; then
         if [[ -n "${VERBOSE}" ]]; then
             echo "$1 not picked"
         fi
+        exit 1
+    fi
+}
+
+# Check machine config modules have been picked by MCO
+is_mcp_ready "load-sctp-module-du-ldc"
+is_mcp_ready "disable-chronyd-du-ldc"
+is_mcp_ready "performance-perf-du-ldc"
+
+# Check kernel patch daemonset has been scheduled on all applicable nodes
+DS_MISS=$(oc get ds/rtos-du-ldc-ds -o \
+    jsonpath='{.status.numberMisscheduled}')
+if [[ ${DS_MISS} -gt 0 ]]; then
+    if [[ -n "${VERBOSE}" ]]; then
+        echo "Kernel patch daemonset is not updated yet"
+    fi
     exit 1
 fi
 
-is_mcp_ready("load-sctp-module")
-
-# }
-# # SCTP MC patch
-# if [ -z "$(oc get mcp worker-du-ldc -o jsonpath='{.spec.configuration.source[?(@.name=="load-sctp-module")].name}')" ]; then
-#     exit 1
-# fi
-
-# # Disable chronyd patch
-# if [ -z "$(oc get mcp worker-du-ldc -o jsonpath='{.spec.configuration.source[?(@.name=="disable-chronyd-du-ldc")].name}')" ]; then
-#     exit 1
-# fi
-
-# # Performance profile
-# if [ -z "$(oc get mcp worker-du-ldc -o jsonpath='{.spec.configuration.source[?(@.name=="performance-perf-du-ldc")].name}')" ]; then
-#     exit 1
-# fi
-
-
-# oc wait mcp/worker-cnf --for condition=updated --timeout 1s
+# Check MCP is updated
+oc wait mcp/worker-du-ldc --for condition=updated --timeout 1s
