f2c2f2cc8b6a7c590bc06fb3c2a5683889b693b0
Vitaly Grinberg <vgrinber@redhat.com>
2020-12-22 18:11:11 +0200

    clone

--- feature-configs/cn-ran-overlays/utils/clone.sh
+++ feature-configs/cn-ran-overlays/utils/clone.sh
@@ -15,21 +15,27 @@ if [[ -z $1 || -z $2 || -z $3 ]]; then
   exit 1
 fi
 
+if [[ ! -d $1/$2 ]]; then
+  echo "ERROR: The source path provided does not exest"
+  exit 1
+fi
+
+# Clone the source directory under the new name
+cp -rf $1/$2 $1/$3
+
+# Rename all files that contain the old flavor in their name
+RN_FILES=$(find $1/$3 -type f -name *$2*)
+for f in $RN_FILES; do
+  FN=$(basename $f)
+  NEW_FN=$(echo $FN | sed -e s/$2/$3/)
+  mv $f $(dirname $f)/$NEW_FN
+done
+
 # Network resource can't contain dashes
 # We replace them by underscores
 NR_FROM=$(echo $2 | sed -e 's/-/_/')
 NR_TO=$(echo $3 | sed -e 's/-/_/')
-
-cp -rf $1/$2 $1/$3
-
-find $1/$3 -type f -exec sed -i -e 's/$2/$3/g' {} +
-find $1/$3 -type f -exec sed -i -e 's/$NR_FROM/$NR_TO/g' {} +
-
-RN_FILES=$(find $1/$3 -type f -name *$2*)
-for f in $RN_FILES; do 
-  PN=$(dirname $f)
-  FN=$(basename $f)
-  NEW_FN=$(echo $FN | sed -e s/$2/$3/)
-  mv $f $PN/$NEW_FN
-done
+find $1/$3 -type f -exec sed -i -e s/$2/$3/g {} +
+find $1/$3 -type f -exec sed -i -e s/$NR_FROM/$NR_TO/g {} +
+echo "Done"
 
