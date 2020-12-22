89ed9ffe656f668a766240d497b68d2d6fc6d3f7
Vitaly Grinberg <vgrinber@redhat.com>
2020-12-22 11:38:51 +0200

    is_rdy

--- feature-configs/cn-ran-overlays/du-ldc/is_ready.sh
+++ feature-configs/cn-ran-overlays/du-ldc/is_ready.sh
@@ -2,6 +2,8 @@
 #
 # Check DU-LDC deployment / configuration are complete
 
+CONFIGURATION="du-ldc"
+ROLE="worker-$CONFIGURATION"
 
 ####################################################
 # Checks whether the machine config pool contains a 
@@ -19,28 +21,42 @@ function is_mcp_ready ()
     MODULE=\"$1\"
     QUERY="{.spec.configuration.source[?(@.name==$MODULE)].name}"
     # If module name is not present in the MCP, abort (not ready)
-    if [ -z "$(oc get mcp worker-du-ldc -o jsonpath=$QUERY)" ]; then
-        if [[ -n "${VERBOSE}" ]]; then
-            echo "$1 not picked"
-        fi
-        exit 1
+    if [ -z "$(oc get mcp $ROLE -o jsonpath=$QUERY)" ]; then
+        abort "$1 not picked"
     fi
 }
 
-# Check machine config modules have been picked by MCO
-is_mcp_ready "load-sctp-module-du-ldc"
-is_mcp_ready "disable-chronyd-du-ldc"
-is_mcp_ready "performance-perf-du-ldc"
 
-# Check kernel patch daemonset has been scheduled on all applicable nodes
-DS_MISS=$(oc get ds/rtos-du-ldc-ds -o \
-    jsonpath='{.status.numberMisscheduled}')
-if [[ ${DS_MISS} -gt 0 ]]; then
+function abort ()
+{
     if [[ -n "${VERBOSE}" ]]; then
-        echo "Kernel patch daemonset is not updated yet"
+            echo "$1"
     fi
     exit 1
+}
+
+# Check machine config modules have been picked by MCO
+is_mcp_ready "load-sctp-module-$CONFIGURATION"
+is_mcp_ready "disable-chronyd-$CONFIGURATION"
+is_mcp_ready "performance-perf-$CONFIGURATION"
+
+# Check kernel patch daemonset has been scheduled on all applicable nodes
+DS_MISS=$(oc get ds/rtos-$CONFIGURATION-ds -o \
+    jsonpath='{.status.numberMisscheduled}')
+if [[ ${DS_MISS} -gt 0 ]]; then
+    abort "Kernel patch daemonset is not updated yet"
 fi
+
+# Check hernel has been patched on all machines
+LST_KERNELS=$(oc get no -l node-role.kubernetes.io/$ROLE="" -o json \
+    |grep '"kernelVersion": ')
+IFS=,
+for value in $LST_KERNELS;
+do
+    if [[ -z $(echo $value |grep rt) ]]; then
+       abort "Kernel has not been patched yet on all machines"
+    fi
+done
 
 # Check MCP is updated
 oc wait mcp/worker-du-ldc --for condition=updated --timeout 1s
