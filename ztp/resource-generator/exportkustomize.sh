dc67cca1b996cc40a36c83eb88176da570a96208
Jim Ramsay <jramsay@redhat.com>
2022-01-27 10:11:15 -0500

    ztp: Remove older 'exportkustomize.sh' in favor of shared 'extract' utility
    This should be operationally the same, but removes some code
    duplication.
    
    Signed-off-by: Jim Ramsay <jramsay@redhat.com>

--- ztp/resource-generator/exportkustomize.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# This shell script is an entrypoint in the container used to patch ArgoCD kustomize with the SiteConfig and PolicyGen plugins.
-
-destPath=$1
-
-cp -r /kustomize $destPath
