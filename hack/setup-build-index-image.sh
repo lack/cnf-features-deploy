4faf3be00055a46966afad88888566a60670afbe
Jim Ramsay <jramsay@redhat.com>
2022-02-03 13:06:52 -0500

    infra: Improve hack/setup-build-index-image.sh
    This cleans up the script a bit, and provides a couple escape hatches if
    things go wrong for earlier detection.
    
    Signed-off-by: Jim Ramsay <jramsay@redhat.com>

--- hack/setup-build-index-image.sh
+++ hack/setup-build-index-image.sh
@@ -4,15 +4,16 @@ set -e
 . $(dirname "$0")/common.sh
 
 #Note: build the index image for all the images we need
-dockercgf=`oc -n openshift-marketplace get sa builder -oyaml | grep imagePullSecrets -A 1 | grep -o "builder-.*"`
+dockercfg=`${OC_TOOL} -n openshift-marketplace get sa builder -oyaml | grep imagePullSecrets -A 1 | grep -o "builder-.*"`
 
 export OPERATOR_RELEASE="release-${OPERATOR_VERSION}"
 
 # TODO: improve the script
-jobdefinition='apiVersion: v1
+jobname=build-ci-index
+jobdefinition="apiVersion: v1
 kind: Pod
 metadata:
-  name: podman
+  name: $jobname
   namespace: openshift-marketplace
 spec:
   restartPolicy: Never
@@ -30,43 +31,45 @@ spec:
           wget https://github.com/operator-framework/operator-registry/releases/download/v1.19.0/linux-amd64-opm
           mv linux-amd64-opm opm
           chmod +x ./opm
-          export pass=$( jq .\"image-registry.openshift-image-registry.svc:5000\".password /var/run/secrets/openshift.io/push/.dockercfg )
-          podman login -u serviceaccount -p ${pass:1:-1} image-registry.openshift-image-registry.svc:5000 --tls-verify=false
+          pass=\$( jq .\"image-registry.openshift-image-registry.svc:5000\".password /var/run/secrets/openshift.io/push/.dockercfg )
+          [[ \$? -eq 0 ]]
+          export pass
+          podman login -u serviceaccount -p \${pass:1:-1} image-registry.openshift-image-registry.svc:5000 --tls-verify=false
 
-          git clone --single-branch --branch OPERATOR_RELEASES https://github.com/openshift/sriov-network-operator.git
+          git clone --single-branch --branch $OPERATOR_RELEASE https://github.com/openshift/sriov-network-operator.git
           cd sriov-network-operator
           podman build -f bundleci.Dockerfile --tag image-registry.openshift-image-registry.svc:5000/openshift-marketplace/sriov-operator-bundle:latest .
           podman push image-registry.openshift-image-registry.svc:5000/openshift-marketplace/sriov-operator-bundle:latest --tls-verify=false
           cd ..
 
-          git clone --single-branch --branch OPERATOR_RELEASES https://github.com/openshift/ptp-operator.git
+          git clone --single-branch --branch $OPERATOR_RELEASE https://github.com/openshift/ptp-operator.git
           cd ptp-operator
           podman build -f bundleci.Dockerfile --tag image-registry.openshift-image-registry.svc:5000/openshift-marketplace/ptp-operator-bundle:latest .
           podman push image-registry.openshift-image-registry.svc:5000/openshift-marketplace/ptp-operator-bundle:latest --tls-verify=false
           cd ..
 
 
-          git clone --single-branch --branch OPERATOR_RELEASES https://github.com/openshift/special-resource-operator.git
-          cd special-resource-operator/bundle/OPERATOR_VERSION/
+          git clone --single-branch --branch $OPERATOR_RELEASE https://github.com/openshift/special-resource-operator.git
+          cd special-resource-operator/bundle/$OPERATOR_VERSION/
           podman build -f bundle.Dockerfile --tag image-registry.openshift-image-registry.svc:5000/openshift-marketplace/special-resource-operator-bundle:latest .
           podman push image-registry.openshift-image-registry.svc:5000/openshift-marketplace/special-resource-operator-bundle:latest --tls-verify=false
           cd ../../..
 
-          git clone --single-branch --branch OPERATOR_RELEASES https://github.com/openshift/cluster-nfd-operator.git
-          cd cluster-nfd-operator/manifests/OPERATOR_VERSION/
+          git clone --single-branch --branch $OPERATOR_RELEASE https://github.com/openshift/cluster-nfd-operator.git
+          cd cluster-nfd-operator/manifests/$OPERATOR_VERSION/
           podman build -f bundle.Dockerfile --tag image-registry.openshift-image-registry.svc:5000/openshift-marketplace/cluster-nfd-operator-bundle:latest .
           podman push image-registry.openshift-image-registry.svc:5000/openshift-marketplace/cluster-nfd-operator-bundle:latest --tls-verify=false
           cd ../../..
 
 
-          git clone --single-branch --branch OPERATOR_RELEASES https://github.com/openshift/metallb-operator.git
+          git clone --single-branch --branch $OPERATOR_RELEASE https://github.com/openshift/metallb-operator.git
           cd metallb-operator
           podman build -f bundleci.Dockerfile --tag image-registry.openshift-image-registry.svc:5000/openshift-marketplace/metallb-operator-bundle:latest .
           podman push image-registry.openshift-image-registry.svc:5000/openshift-marketplace/metallb-operator-bundle:latest --tls-verify=false
           cd ..
 
 
-          git clone --single-branch --branch GATEKEEPER_VERSION https://github.com/open-cluster-management/gatekeeper-operator.git
+          git clone --single-branch --branch $GATEKEEPER_VERSION https://github.com/open-cluster-management/gatekeeper-operator.git
           cd gatekeeper-operator
           podman build -f bundle.Dockerfile --tag image-registry.openshift-image-registry.svc:5000/openshift-marketplace/gatekeeper-operator-bundle:latest .
           podman push image-registry.openshift-image-registry.svc:5000/openshift-marketplace/gatekeeper-operator-bundle:latest --tls-verify=false
@@ -84,16 +87,18 @@ spec:
     - name: dockercfg
       defaultMode: 384
       secret:
-      '
+        secretName: ${dockercfg}
+"
 
-jobdefinition=$(sed "s#OPERATOR_VERSION#${OPERATOR_VERSION}#" <<< "$jobdefinition")
-jobdefinition=$(sed "s#OPERATOR_RELEASES#${OPERATOR_RELEASE}#" <<< "$jobdefinition")
-jobdefinition=$(sed "s#GATEKEEPER_VERSION#${GATEKEEPER_VERSION}#" <<< "$jobdefinition")
+if ${OC_TOOL} get -n openshift-marketplace pod $jobname &>/dev/null; then
+    echo "Removing stale job $jobname"
+    ${OC_TOOL} delete -n openshift-marketplace pod $jobname
+fi
 
-
-jobdefinition="${jobdefinition} secretName: ${dockercgf}"
+echo "Applying job $jobname:"
+echo
 echo "$jobdefinition"
-echo "$jobdefinition" | ${OC_TOOL} apply -f -
+${OC_TOOL} apply -f - <<<${jobdefinition}
 
 success=0
 iterations=0
@@ -101,12 +106,20 @@ sleep_time=10
 max_iterations=72 # results in 12 minutes timeout
 until [[ $success -eq 1 ]] || [[ $iterations -eq $max_iterations ]]
 do
-  run_status=$(${OC_TOOL} -n openshift-marketplace get pod podman -o json | jq '.status.phase' | tr -d '"')
-   if [ $run_status == "Succeeded" ]; then
+  run_status=$(${OC_TOOL} -n openshift-marketplace get pod $jobname -o json | jq '.status.phase' | tr -d '"')
+  case $run_status in
+      Succeeded)
           success=1
           break
-   fi
+          ;;
+      Failed)
+          break
+          ;;
+  esac
 done
+
+# print the build logs
+${OC_TOOL} -n openshift-marketplace logs $jobname
 
 if [[ $success -eq 1 ]]; then
   echo "[INFO] index build succeeded"
@@ -114,9 +127,6 @@ else
   echo "[ERROR] index build failed"
   exit 1
 fi
-
-# print the build logs
-${OC_TOOL} -n openshift-marketplace logs podman
 
 #Note: adding a CI index image
 cat <<EOF | ${OC_TOOL} apply -f -
